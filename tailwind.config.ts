ce63850df9c860c7362c8224ba4f6f5d946c4a6b
Rick Floyd <forexanarchy@gmail.com>
2025-11-05 10:38:39 +0000

    CHANGE THE COLOR FROM WHITE TO BLUE INSIDE THE DROP DOWN MENU

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,12 +59,12 @@ export default {
         sidebar: {
           DEFAULT: 'hsl(var(--sidebar-background))',
           foreground: 'hsl(var(--sidebar-foreground))',
+          border: 'hsl(var(--sidebar-border))',
+          ring: 'hsl(var(--sidebar-ring))',
           primary: 'hsl(var(--sidebar-primary))',
           'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
           accent: 'hsl(var(--sidebar-accent))',
           'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-          border: 'hsl(var(--sidebar-border))',
-          ring: 'hsl(var(--sidebar-ring))',
         },
       },
       borderRadius: {
